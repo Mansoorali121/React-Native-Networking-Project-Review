82da2ad4943f860beb3c6936292b9d192010f2e1
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-09 16:20:05 +0500

    book add

--- App.tsx
+++ App.tsx
@@ -1,23 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import { Alert, Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
   const getBooks = async () => {
-    const response = await axios.get("https://69609023e7aa517cb79661a7.mockapi.io/Books")
-    console.log(JSON.stringify(response.data, null, 3))
-  }
-  useEffect(()=>{
+    const response = await axios.get(
+      'https://69609023e7aa517cb79661a7.mockapi.io/Books',
+    );
+    console.log(JSON.stringify(response.data, null, 3));
+  };
+  // Adding Book
+  const addBook = async () => {
+    const response = await axios.post(
+      'https://69609023e7aa517cb79661a7.mockapi.io/Books',
+      {
+        author: 'Mansoor',
+        cover: 'https://picsum.photos/seed/fXytBvu5g5/1509/3939',
+        name_of_book: 'Fifty Days',
+        id: '6',
+      },
+    );
+    console.log(response.data);
+    Alert.alert("Book wad Added Successfully ")
+  };
+  useEffect(() => {
     getBooks();
+  }, []);
 
-  },[])
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Pressable onPress={addBook}
+        style={{
+          backgroundColor: 'red',
+          paddingHorizontal: 20,
+          padding: 14,
+          borderRadius: 20,
+        }}
+      >
+        <Text style={{ fontSize: 14, color: '#fff', fontWeight: 'bold' }}>
+          Add Book
+        </Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
 export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
