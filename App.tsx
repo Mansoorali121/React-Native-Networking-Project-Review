cb1621a06e13338af7dc05ea4041ffbe50807c83
Mansoor Ali <mansoorsahito601@gmail.com>
2026-01-09 11:12:53 +0500

    request fetched

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const getBooks = async () => {
+    const response = await axios.get("https://69609023e7aa517cb79661a7.mockapi.io/Books")
+    console.log(JSON.stringify(response.data, null, 3))
+  }
+  useEffect(()=>{
+    getBooks();
+
+  },[])
   return (
     <View>
       <Text>App</Text>
