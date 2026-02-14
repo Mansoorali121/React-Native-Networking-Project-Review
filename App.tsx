bb8bbd91e6d57b6d309271878c307e711c0ead1e
Mansoor Ali <mansoorsahito601@gmail.com>
2026-02-14 09:12:28 +0500

    Redux Counter App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import CounterScreen from './src/screens/CounterScreen';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <Provider store={store}>
+      <CounterScreen />
+    </Provider>
+  );
+};
 
 export default App;
